f59fa98205dc9b7edf11c2dc051384479ced0ab2
Randy Ellis <randy.ellis.pro@gmail.com>
2015-07-08 17:28:44 -0500

    Initial Commit

---
+++ config.js
@@ -0,0 +1,11 @@
+// Howdy! This is your website's configuration file.
+// Just replace my personal info with yours, save, and you're done :)
+
+dribbbox.config = {
+
+  dribbble_username : "randyellis",
+  short_description : "Founder of @localluxapp and @5ivehat | citizen of @appsterdamchi | Public Speaker | Visual Designer | Product Design |
+",
+  email_address     : "randy.ellis.pro@gmail.com"
+
+}
